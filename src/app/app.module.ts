aa2205b36f7fd07787ef29ac6d7eb85d579774aa
Matheas <tmatheas@gmail.com>
2020-07-29 10:02:05 +0530

    assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import {ReactiveFormsModule } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
 
 
 @NgModule({
@@ -64,7 +65,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     ReactiveFormsModule,
     MatSlideToggleModule,
     MatSelectModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   entryComponents:[LoginComponent],
   providers: [DishService,PromotionService,LeaderService],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,25 +1,65 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 import {Dish} from '../shared/dish';
 import {Params,ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {DishService} from '../services/dish.service';
 import {switchMap} from 'rxjs/operators';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {Comment} from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
+
 export class DishdetailComponent implements OnInit {
 
   dish:Dish;
   dishIds:string[];
   prev:string;
   next:string;
+  commentForm:FormGroup;
+  comment:Comment;
+
+  @ViewChild('fform') commentFormDirective;
+
+  formErrors={
+    'author':'',
+    'rating':'',
+    'comment':''
+  };
+  
+  validationMessages={
+    'author':{
+      'required':'Name is required.',
+      'minlength':'Name must be atleast 2 characters long.',
+      'maxlength':'Name cannot be more than 25 characters long.'
+    },
+    'rating':{
+      'required':'Rating is required.'
+    },
+    'comment':{
+      'required':'Comment is required.'
+    }
+  };
+
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 1;
+    }
+    if (value > 5) {
+      return Math.round(value / 5);
+    }
+    return value;
+  }
     
   constructor(private dishService:DishService,
     private location:Location,
-    private route:ActivatedRoute) { }
+    private route:ActivatedRoute,
+    private fb:FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
     this.dishService.getDishIds().subscribe((dishIds)=>{
@@ -41,6 +81,57 @@ export class DishdetailComponent implements OnInit {
 
   goBack():void{
     this.location.back();
+  }
+
+  createForm(){
+    this.commentForm=this.fb.group({
+      author:['',[Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      rating:['',Validators.required],
+      comment:['',Validators.required]
+    });
+
+    this.commentForm.valueChanges.subscribe(data =>this.onValueChanged(data));
+    this.onValueChanged();
+  }
+
+  onSubmit(){
+    let d = new Date();
+    let n = d.toISOString();
+    this.comment=this.commentForm.value;
+    this.comment.date=n;
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      author:'',
+      rating:'',
+      comment:''
+    });
+    this.commentFormDirective.resetForm();
+  }
+
+  onValueChanged(data?:any){
+    if(!this.commentForm)
+    {
+      return ;
+    }
+    const form=this.commentForm;
+    for (const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field))
+      {
+        this.formErrors[field]='';
+        const control=form.get(field);
+        if(control && control.dirty && !control.valid)
+        {
+          const messages=this.validationMessages[field];
+          for(const key in control.errors)
+          {
+            if(control.errors.hasOwnProperty(key))
+            {
+              this.formErrors[field]+=messages[key]+' ';
+            }
+          }
+        }
+      }
+    }
   }
 
 }
